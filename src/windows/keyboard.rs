663e95d7155d4c0b23b953e031a331731f56b7f8
ChristopherAttilano <34689512+ChristopherAttilano@users.noreply.github.com>
2021-01-22 10:19:08 -0500

    Update keyboard.rs

--- src/windows/keyboard.rs
+++ src/windows/keyboard.rs
@@ -2,7 +2,7 @@
 use crate::windows::common::*;
 
 pub const HOTKEY:u32 = 65;
-
+pub static mut BLOCK_KEYBOARD:bool = true;
 lazy_static! {
     pub static ref KEYBOARD_EVENT_CHANNEL: (Mutex<Sender<KBDLLHOOKSTRUCT>>, Mutex<Receiver<KBDLLHOOKSTRUCT>>) = {
         let (send, recv) = unbounded();
@@ -13,16 +13,25 @@ lazy_static! {
         (Mutex::new(send), Mutex::new(recv))
     };
     pub static ref KEYBD_HHOOK: Lazy<AtomicPtr<HHOOK__>> = Lazy::new(AtomicPtr::default);
-    pub static ref BLOCK_KEYBOARD:bool = true;
+    
 }
-
+pub fn get_keyboard_recv() -> Receiver<KBDLLHOOKSTRUCT>{
+    KEYBOARD_EVENT_CHANNEL
+        .1
+        .lock()
+        .expect("Failed to unlock Mutex")
+        .clone()
+}
+pub unsafe fn set_block_keyboard(b:bool){
+     BLOCK_KEYBOARD = b;
+}
 pub fn receive_keyboard_event(){
-    thread::spawn(move || {
-        let recv = KEYBOARD_EVENT_CHANNEL.1.lock().expect("Failed to unlock Mutex");
-        for key_event_struct in recv.iter() {
-            println!("key_event_struct: code: {:?},  scanCode: {:?}, flags: {:?}, time: {:?}, extra: {:?},",key_event_struct.vkCode, key_event_struct.scanCode,key_event_struct.flags,key_event_struct.time,key_event_struct.dwExtraInfo);
-        }
-    });
+    // thread::spawn(move || {
+    //     let recv = KEYBOARD_EVENT_CHANNEL.1.lock().expect("Failed to unlock Mutex");
+    //     for key_event_struct in recv.iter() {
+    //         println!("key_event_struct: code: {:?},  scanCode: {:?}, flags: {:?}, time: {:?}, extra: {:?},",key_event_struct.vkCode, key_event_struct.scanCode,key_event_struct.flags,key_event_struct.time,key_event_struct.dwExtraInfo);
+    //     }
+    // });
     thread::spawn(move || {
         let recv = TERMINATEPROGRAM.1.lock().expect("Failed to unlock Mutex");
         for _ in recv.iter() {
@@ -43,13 +52,7 @@ pub fn receive_keyboard_event(){
 }
 
 pub unsafe extern "system" fn keyboard_hook_callback(code: i32, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
-    let keyboard_event_struct = (*(lParam as *const KBDLLHOOKSTRUCT));
-    KEYBOARD_EVENT_CHANNEL
-        .0
-        .lock()
-        .expect("Failed to unlock Mutex")
-        .send(keyboard_event_struct)
-        .expect("Receiving end of KEYBOARD_EVENT_CHANNEL was closed");
+    let mut keyboard_event_struct = (*(lParam as *const KBDLLHOOKSTRUCT));
     if keyboard_event_struct.vkCode == HOTKEY{
         TERMINATEPROGRAM
         .0
@@ -58,11 +61,52 @@ pub unsafe extern "system" fn keyboard_hook_callback(code: i32, wParam: WPARAM, 
         .send(true)
         .expect("Receiving end of TERMINATEPROGRAM was closed");
     }
-    if *BLOCK_KEYBOARD {
-        return 1
-    }else {
+    // if keyboard_event_struct.vkCode == 66{
+    //     if keyboard_event_struct.flags == 0 {
+    //        send_keybd_input(KEYEVENTF_SCANCODE , 67); 
+    //     }  
+    // }
+    println!("IMPORTANT!!!: code: {:?},  scanCode: {:?}, flags: {:?}, time: {:?}, extra: {:?},",keyboard_event_struct.vkCode, keyboard_event_struct.scanCode,keyboard_event_struct.flags,keyboard_event_struct.time,keyboard_event_struct.dwExtraInfo);
+        
+    if keyboard_event_struct.dwExtraInfo == 1 || !BLOCK_KEYBOARD {
+        // println!("sent event!");
         return 0
+    }else { 
+        // if wParam as u32 == WM_KEYDOWN {
+        //     keyboard_event_struct.flags = 0;
+        // }else{
+        //     keyboard_event_struct.flags = 128;
+        // }
+        KEYBOARD_EVENT_CHANNEL  
+        .0
+        .lock()
+        .expect("Failed to unlock Mutex")
+        .send(keyboard_event_struct)
+        .expect("Receiving end of KEYBOARD_EVENT_CHANNEL was closed");
+        
+        
+        return 1
     }
+    
+    // if *BLOCK_KEYBOARD {
+    //     return 1
+    // }
+}
+
+pub fn send_keybd_input(scan_code: u32, key_code: u32, flags: u32) {
+    let mut input = INPUT {
+        type_: INPUT_KEYBOARD,
+        u: unsafe {
+            transmute_copy(&KEYBDINPUT {
+                wVk: key_code as u16,
+                wScan: scan_code as u16,
+                dwFlags: flags,
+                time: 0,
+                dwExtraInfo: 1,
+            })//MapVirtualKeyW(key_code, 0) as u16
+        },
+    };
+    unsafe { SendInput(1, &mut input as LPINPUT, size_of::<INPUT>() as c_int) };
 }
 
 // fn listen_to_keyboard_event(){
